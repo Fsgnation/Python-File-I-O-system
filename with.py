e30d36f12de7b1d4c6ab9e1f143a5daeaeccebbd
Fsgnation <anshsingh44698@gmail.com>
2025-03-30 15:44:01 +0530

    Update with.py

--- with.py
+++ with.py
@@ -1,4 +1,11 @@
 ''' With Statement '''
 
 #generally we use 
+f=open("file.txt")
+data=f.read()
+f.close()
 
+#now we can write this same logic with with statement, traditonally we have to close file also and sometime we write bunch line we can miss it 
+
+with open("file.txt") as f:
+    print(f.read())
