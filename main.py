0834a11f2251ed9ae598dbc934824e4edc22b513
Fsgnation <anshsingh44698@gmail.com>
2025-03-30 15:54:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,42 @@ emails = "alpha_test123@fakemail.com\nrandom_user567@nomail.net\nghostemail999@t
 x=open("emails.txt","w") # here mode is w for writing in a file/ in my pc emials.txt doesnt exist but it will crerate / to insert in a existing file we use a mode where a is append
 x.write(emails)
 x.close()
+
+# using this we can print all lines in the form of a list
+
+b=open("poem.txt")
+line=b.readlines()
+print(b,type(b))
+
+f=open("file.txt")
+
+line1=f.readline() # we can name var of your wish it isnt neccesary to give line1 the no of funvtion is called that number of list repeateed 
+print(line1,type(line1))
+
+line2=f.readline()
+print(line2,type(line2))
+
+line3=f.readline()
+print(line3,type(line3))
+
+line4=f.readline()
+print(line4,type(line4))
+
+line5=f.readline()
+print(line5,type(line5))
+
+line6=f.readline()
+print(line6,type(line6)) # our txt doesnt contain 6th line but it will not raise a errror instead of that it will give nad empty string 
+
+print(line6=="")#this will give True
+
+f.close()
+
+# doing same thing by loop
+
+f=open("file.txt")
+line=f.readline()
+
+while(line!=""):
+    print(line)
+    line=f.readline()
