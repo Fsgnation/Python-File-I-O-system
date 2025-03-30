b51bebebdb70a4e3a8a85051bdb55acd674b107e
Fsgnation <anshsingh44698@gmail.com>
2025-03-30 15:51:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ f=open("file.txt") #<--- it  contins two parameters path of file then mode , mod
 data=f.read()
 f.close()
 print(data)
+
+''' writng in a file /can create file using this w mode'''
+emails = "alpha_test123@fakemail.com\nrandom_user567@nomail.net\nghostemail999@tempmail.org\nfake_address101@mockmail.xyz\ntrial_mailer456@noemail.com\nnonexistent987@randommail.net\ndummyemail2024@fakemail.org\ntesting_acc001@madeup.com\nsample_mail789@notreal.net\ntestuserX23@nowhere.xyz"
+# here emails is a string conatianing some sample emails .
+
+x=open("emails.txt","w") # here mode is w for writing in a file/ in my pc emials.txt doesnt exist but it will crerate / to insert in a existing file we use a mode where a is append
+x.write(emails)
+x.close()
